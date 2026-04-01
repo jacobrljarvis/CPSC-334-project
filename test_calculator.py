2a71e3823dcaef3a79ae5225de16db869e856204
jacobrljarvis <jacobrljarvis@gmail.com>
2026-04-01 10:57:00 -0700

    added tests and simple add and subtract

---
+++ test_calculator.py
@@ -0,0 +1,37 @@
+import pytest
+from calculator import Calculator
+
+
+@pytest.fixture
+def calc():
+    return Calculator()
+
+
+# --- Addition ---
+
+def test_add_two_positives(calc):
+    assert calc.add(2, 3) == 5
+
+def test_add_negative(calc):
+    assert calc.add(-1, 4) == 3
+
+def test_add_two_negatives(calc):
+    assert calc.add(-2, -3) == -5
+
+def test_add_zero(calc):
+    assert calc.add(7, 0) == 7
+
+
+# --- Subtraction ---
+
+def test_subtract_basic(calc):
+    assert calc.subtract(10, 4) == 6
+
+def test_subtract_negative_result(calc):
+    assert calc.subtract(3, 7) == -4
+
+def test_subtract_negative_number(calc):
+    assert calc.subtract(5, -2) == 7
+
+def test_subtract_zero(calc):
+    assert calc.subtract(5, 0) == 5
